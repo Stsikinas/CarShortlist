2a827613b51579402d493d3b30f6953dd0188730
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-12-02 17:42:56 +0200

    initial setup

---
+++ CarShortlist/CarShortlist/Views/VC/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarShortlist
+//
+//  Created by Epsilon User on 2/12/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
